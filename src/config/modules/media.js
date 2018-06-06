d10e15ad0bfe5f1ac087f663844521bf19475e06
lonelydawn <lonelydawn@sina.com>
2018-06-06 12:35:27 +0800

    vue企业官网篇终结

---
+++ src/config/modules/media.js
@@ -0,0 +1,24 @@
+/**
+ * Created by lonelydawn on 2018-04-19.
+ */
+
+export default {
+  chinese: {
+    SLOGAN_PATH: './static/images/rent_car_70.png',
+    STEPS_ACTOR_PATH: [
+      './static/images/rent_car_52.png',
+      './static/images/rent_car_54.png',
+      './static/images/rent_car_56.png',
+      './static/images/rent_car_57.png'
+    ]
+  },
+  english: {
+    SLOGAN_PATH: './static/images/rent_car_69.png',
+    STEPS_ACTOR_PATH: [
+      './static/images/rent_car_51.png',
+      './static/images/rent_car_53.png',
+      './static/images/rent_car_55.png',
+      './static/images/rent_car_57.png'
+    ]
+  }
+}
